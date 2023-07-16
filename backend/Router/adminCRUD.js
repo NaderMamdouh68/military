17033208265b7df9b287bd129755e42af0a606bc
Nader Mamdouh <74846059+NaderMamdouh68@users.noreply.github.com>
2023-07-17 01:56:13 +0300

    nader

---
+++ backend/Router/adminCRUD.js
@@ -0,0 +1,58 @@
+import express from "express";
+import query from '../Database/DBConnection.js';
+import { body, validationResult } from "express-validator";
+
+const admin = express();
+admin.use(express.Router());
+
+admin.get('/allstudent',
+    async (req, res) => {
+        try {
+            const result = await query('SELECT student.student_name , student.student_national_id , material.material_name , material.material_code , material.material_sim , material.material_status , grade.year_work , grade.full_grade , grade.practical_exams_grade	, grade.written_exams_grade FROM students AS student INNER JOIN grade ON student.student_id = grade.student_id INNER JOIN material ON grade.material_id = material.material_id');
+            res.status(200).send(result);
+
+
+        } catch (error) {
+            console.log(error);
+        }
+    }
+);
+
+
+admin.get('/allmaterial',
+    async (req, res) => {
+        try {
+            const result = await query('SELECT * FROM material');
+            res.status(200).send(result);
+
+        } catch (error) {
+            console.log(error);
+        }
+    }
+);
+
+admin.get('/getmaterial/:id',
+    async (req, res) => {
+        try {
+            const result = await query('SELECT material.* , grade.year_work , grade.full_grade , grade.practical_exams_grade	, grade.written_exams_grade , students.student_name , students.student_national_id FROM material INNER JOIN grade ON material.material_id = grade.material_id INNER JOIN students ON grade.student_id = students.student_id WHERE material.material_id = ?', [req.params.id]);
+            if (result.length > 0) {
+                res.status(200).send(result);
+            }else{
+                res.status(404).send("Not Found");
+            }
+
+        } catch (error) {
+            console.log(error);
+        }
+    }
+);
+
+
+
+
+
+
+
+
+
+export default admin;
---
+++ backend/index.js
@@ -0,0 +1,48 @@
+import express from 'express';
+import cors from 'cors';
+import dotenv from 'dotenv';
+import bodyParser from 'body-parser';
+import cookieParser from 'cookie-parser';
+import session from 'express-session';
+import admin from './Router/adminCRUD.js';
+
+
+
+
+const app = express();
+app.use(cors({
+    origin: ['http://localhost:3000'],
+    methods: ['GET', 'POST', 'PUT', 'DELETE'],
+    credentials: true,
+    maxAge: 1 * 24 * 60 * 60 * 1000,
+    optionsSuccessStatus: 200
+}));
+
+app.use(session({
+    secret: 'secret',
+    resave: false,
+    saveUninitialized: false,
+    cookie: {
+        secure: false,
+    }
+}));
+
+
+
+app.use(express.json());
+app.use(cookieParser());
+app.use(bodyParser.json());
+app.use(express.urlencoded({ extended: true }));
+
+
+app.use('/admin', admin);
+
+
+
+dotenv.config({ path: './.env' });
+const PORT = process.env.PORT || 5000;
+
+
+app.listen(PORT, () => {
+    console.log("Server is running on port 5000");
+});
